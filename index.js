faeff9c15e4a85ef22c245305b88707d100cd866
leibo <superviho1@gmail.com>
2021-12-16 20:12:23 +0800

    简化

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ require.config({
 });
 require(["loader", "urlSearchParse"], function(AMapLoader, urlSearchParse) {
   var version = urlSearchParse("v");
-  var key = urlSearchParse('key') || 'ba5f2d926220e97b20cc496328990682';
+  var key = urlSearchParse("key") || "ba5f2d926220e97b20cc496328990682";
   var config = {
     key: key, // 申请好的Web端开发者Key，首次调用 load 时必填
     plugins: [
@@ -29,17 +29,14 @@ require(["loader", "urlSearchParse"], function(AMapLoader, urlSearchParse) {
       plugins: ["overlay/SimpleMarker"] // 需要加载的 AMapUI ui插件
     }
   };
-  var extraConfig =
-    version === "2"
-      ? {
-          version: "2.0", // 指定要加载的 JSAPI 的版本，缺省时默认为 1.4.15
-          Loca: {
-            // 是否加载 Loca， 缺省不加载
-            version: "2.0.0" // Loca 版本，缺省 1.3.2
-          }
-        }
-      : {};
-  AMapLoader.load(Object.assign({}, config, extraConfig))
+  if (version === "2") {
+    config.version = "2.0"; // 指定要加载的 JSAPI 的版本，缺省时默认为 1.4.15
+    config.Loca = {
+      // 是否加载 Loca， 缺省不加载
+      version: "2.0.0" // Loca 版本，缺省 1.3.2
+    };
+  }
+  AMapLoader.load(config)
     .then(function(AMap) {
       new AMap.Map("container");
     })
